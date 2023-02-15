aa53d6071a4323a5458efdcfa179a690f652f115
VSantiM <63769445+VSantiM@users.noreply.github.com>
2023-02-15 19:57:23 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ufabc/calculadoras/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.edu.ufabc.calculadoras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val buttonChangeConversor = findViewById<Button>(R.id.buttonChangeConversor)
+        val buttonChangeCalculadora = findViewById<Button>(R.id.buttonChangeCalculadora)
+
+        buttonChangeConversor.setOnClickListener {
+            val myIntent = Intent(this, ConversorActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        buttonChangeCalculadora.setOnClickListener {
+            val myIntent = Intent(this, ConversorActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+}
